5939c268056f44f9dae22e7ddf9c3feb9073fa79
Jochen Wezel <jwezel@compumaster.de>
2016-07-19 22:24:15 +0200

    unboxing of SQL parameters text/ntext
    workaround for [Microsoft][ODBC Driver 11 for SQL Server][SQL Server]Die
    Datentypen 'text', 'ntext' und 'image'
    using varchar/nvarchar(max)

--- TraceConverter.vb
+++ TraceConverter.vb
@@ -6,6 +6,7 @@ Imports System.Text.RegularExpressions
 Public Module TraceConverter
 
     Public Sub Execute(options As TraceConverterOptions)
+        'Read all SQL statements from the trace file via the SQL server trace file filter engine
         Dim MyCmd As New SqlClient.SqlCommand("", New SqlClient.SqlConnection(options.ConnectionString))
         MyCmd.CommandType = CommandType.Text
         MyCmd.CommandText = "SELECT CAST(TextData AS nvarchar(max))" & vbNewLine &
@@ -13,9 +14,9 @@ Public Module TraceConverter
                             "WHERE IsNull(TextData,'') not like ''" & vbNewLine &
                             "    and textdata not like 'exec sp_reset_connection%'" & vbNewLine &
                             "GROUP BY CAST(TextData AS nvarchar(max))"
-
         Dim SqlStatements As List(Of String) = CompuMaster.Data.DataQuery.AnyIDataProvider.ExecuteReaderAndPutFirstColumnIntoGenericList(Of String)(MyCmd, CompuMaster.Data.DataQuery.AnyIDataProvider.Automations.AutoOpenAndCloseAndDisposeConnection)
         System.Console.WriteLine(SqlStatements.Count & " unique SQL statements found")
+        'Unbox all dynamic SQLs into static ones if requested by commandline parameters
         If options.UnboxSpExecuteSql Then
             Dim MyRegEx As New System.Text.RegularExpressions.Regex("exec sp_executesql N'(?<sql>.*)',N'(?<declares>[^']*)'(?<params>,@(?<pname>.*)=(?<pvalue>.*))*", RegexOptions.Singleline Or RegexOptions.Compiled Or RegexOptions.IgnoreCase Or RegexOptions.CultureInvariant)
             For MyCounter As Integer = 0 To SqlStatements.Count - 1
@@ -27,17 +28,20 @@ Public Module TraceConverter
                         Console.WriteLine(sql)
                         Console.WriteLine()
                     End If
-
-                    Dim m As Match = MyRegEx.Match(sql.Replace("''", ChrW(27)))
+                    'Identify inner SQL command, parameter definition, parameter assignment
+                    Dim m As Match = MyRegEx.Match(sql.Replace("''", ChrW(27))) 'escape doubled quotation mark in string to prevent wrong end-of-string-detection by reg-expression
                     Dim Declares As String = m.Groups("declares").Value
                     Dim ParamSelects As New List(Of String)
                     Dim MatchedParams As Group = m.Groups("params")
                     For ParamCounter As Integer = 0 To MatchedParams.Captures.Count - 1
                         ParamSelects.Add(MatchedParams.Captures(ParamCounter).Value)
                     Next
+                    '[Microsoft][ODBC Driver 11 for SQL Server][SQL Server]Die Datentypen 'text', 'ntext' und 'image' sind für lokale Variablen ungültig. 
+                    Declares = Replace(Declares, " text", " varchar(max) --ex: text" & vbNewLine)
+                    Declares = Replace(Declares, " ntext", " nvarchar(max) --ex: ntext" & vbNewLine)
                     sql = "DECLARE " & Declares & vbNewLine &
                     "SELECT " & Strings.Join(ParamSelects.ToArray, "").Substring(1) & vbNewLine &
-                    m.Groups("sql").Value.Replace(ChrW(27), "'")
+                    m.Groups("sql").Value.Replace(ChrW(27), "'") 'change back all previously escaped doubled quotation mark into single quotation mark (single instead of doubled ones because the SQL statement is now a regular static SQL command instead of a string-escaped one for the exec sp_executecommand call
                     If options.VerboseMode Then
                         Console.WriteLine("CONVERTED TO STATIC SQL:")
                         Console.WriteLine(sql)
@@ -49,6 +53,7 @@ Public Module TraceConverter
                 End If
             Next
         End If
+        'Watch out for the output file
         Dim TempFile As String
         If options.SqlFileLocalPath = "" Then
             TempFile = System.IO.Path.GetTempFileName & ".sql"
@@ -56,6 +61,7 @@ Public Module TraceConverter
             TempFile = System.IO.Path.Combine(System.Environment.CurrentDirectory, options.SqlFileLocalPath)
         End If
         System.Console.WriteLine("Writing SQL statements to " & TempFile)
+        'Export into the appropriate output format
         Select Case System.IO.Path.GetExtension(TempFile.ToLowerInvariant)
             Case ".csv"
                 Dim t As DataTable = ConvertToDataTable(SqlStatements)
@@ -73,6 +79,7 @@ Public Module TraceConverter
                 Next
                 System.IO.File.WriteAllText(TempFile, sb.ToString, System.Text.Encoding.Default)
         End Select
+        'Automatically open the exported file if requested by commandline parameters
         If options.AutoOpenSqlFile Then System.Diagnostics.Process.Start(TempFile)
 
     End Sub
